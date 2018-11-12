e03ff6e27bd8f1c02e1773f93f037a9693a4005c
leerichardson <leerichardson2013@gmail.com>
2018-11-11 20:31:24 -0500

    removed tracking, cleaned up the comments and print statements

--- rpm-table.js
+++ rpm-table.js
@@ -1,11 +1,7 @@
-// Global variable to store the dataset (Reccomended from Scott Murray's book), 
-// Because it allows us to use the same data from read.csv in functions called 
-// after the data-set is read in. 
+// Global variable to store dataset (reccomended from Scott Murray's book)
 var dataset;
 
-// Convert the data from the .csv table into the correct types. Specifically, 
-// this means converting the numeric values from their import as strings. Each 
-// row of the .csv file is run through this function 
+// Converts the data from a .csv to an array object with the correc types
 var row_converter = function(d) {
     rounding_scale = 1000;
     return {
@@ -19,10 +15,9 @@ var row_converter = function(d) {
     };
 }
 
-// Function that generates our sortable APM Tables ---
+// Primary funtion to convever .csv file into a sortable table 
 var create_rpm_table = function(filepath) {
     d3.csv(filepath + "?q" + Math.random(), row_converter, function(error, data) {
-        console.log("Filepath: ", filepath);
         dataset = data; // Store dataset in global variable 
 
         // Print out the data-set constructed by d3.csv()
@@ -32,34 +27,29 @@ var create_rpm_table = function(filepath) {
             console.log(data);
         }
 
-        // Adds and empty table to the web page 
-        var table = d3
-                    .select('#page-wrap')
-                    .append("table");
+        // Adds and empty table to webpage 
+        var table = d3.select('#page-wrap')
+                      .append("table");
 
         // Add table headers to the empty table (<thead>)
         var column_titles = d3.keys(data[0]);
         column_titles.pop() // Remove the Standard Error as a column title
-        console.log("Column Titles: ", column_titles);
-        var headers = table
-                        .append("thead")
-                        .append("tr")
-                        .selectAll("th")
-                        .data(column_titles)
-                        .enter()
-                        .append("th")
-                        .text(function (d) {
-                            if (d == "Teams") { return "Team(s)" }
-                            if (d == "Augmented_APM") { return "Augmented APM" }
-                            return d;
-                        })
+        var headers = table.append("thead")
+                           .append("tr")
+                           .selectAll("th")
+                           .data(column_titles)
+                           .enter()
+                           .append("th")
+                           .text(function (d) {
+                                if (d == "Teams") { return "Team(s)" }
+                                if (d == "Augmented_APM") { return "Augmented APM" }
+                                return d;
+                            })
 
-        // Sort by the header value based on the click 
+        // Sort the table column by click 
         sort_ascend = true;
-        headers
-            .on('click', function(h) {
+        headers.on('click', function(h) {
                 console.log("Sorting by: ", h);
-
                 if (sort_ascend) {
                     rows.sort( function(a, b) { return d3.ascending(a[h], b[h]); });                      
                     sort_ascend = false; 
@@ -70,15 +60,14 @@ var create_rpm_table = function(filepath) {
             })
 
         // Add rows the the table (<tr> elements) to <tbody>
-        var rows = table
-                    .append('tbody')
-                    .selectAll('tr')
-                    .data(data)
-                    .enter()
-                    .append('tr')
+        var rows = table.append('tbody')
+                        .selectAll('tr')
+                        .data(data)
+                        .enter()
+                        .append('tr')
 
-        rows
-            .attr("class", "data-tr")
+        // Add the cell values to each table 
+        rows.attr("class", "data-tr")
             .selectAll('td')
             .data(function (d) {
                 return column_titles.map(function (k) { // extracts the name and value 
@@ -118,71 +107,64 @@ var create_rpm_table = function(filepath) {
         var apm_min = d3.min(data, function(d) { return d.Augmented_APM; });
         var apm_max_se = d3.max(data, function(d) { return d.Augmented_APM_SE });
         
-        // console.log("APM MAX: ", apm_max, " APM MIN: ", apm_min, " Max SD: ", apm_max_se);
-        var chart_scale = d3
-                            .scaleLinear()
+        var chart_scale = d3.scaleLinear()
                             .domain([apm_min - (2 * apm_max_se), apm_max + (2 * apm_max_se)])
                             .range([0, svg_chart_width]);
 
-        var color_scale = d3
-                            .scaleLinear()
+        var color_scale = d3.scaleLinear()
                             .domain([apm_min, apm_max])
                             .range(["blue", "red"])                        
 
         // Add the circles to the chart location 
-        var sd_line = chart_location
-                        .append("line")
-                        .attr("x1", function(d) {
-                            return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
-                        })
-                        .attr("x2", function(d) {
-                            return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
-                        })
-                        .attr("y1", svg_chart_height / 2) 
-                        .attr("y2", svg_chart_height / 2)
-                        .attr("stroke", function(d) {
-                            return color_scale(d.APM);
-                        })
+        var sd_line = chart_location.append("line")
+                                    .attr("x1", function(d) {
+                                        return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
+                                    })
+                                    .attr("x2", function(d) {
+                                        return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
+                                    })
+                                    .attr("y1", svg_chart_height / 2) 
+                                    .attr("y2", svg_chart_height / 2)
+                                    .attr("stroke", function(d) {
+                                        return color_scale(d.APM);
+                                    })
 
-        var line_upper = chart_location
-                            .append("line")
-                            .attr("x1", function(d) {
-                                return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
-                            })
-                            .attr("x2", function(d) {
-                                return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
-                            })
-                            .attr("y1", 1 * (svg_chart_height / 4)) 
-                            .attr("y2", 3 * (svg_chart_height / 4))
-                            .attr("stroke", function(d) {
-                                return color_scale(d.Augmented_APM);
-                            })
+        // Add lines for the standard errors 
+        var line_upper = chart_location.append("line")
+                                        .attr("x1", function(d) {
+                                            return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
+                                        })
+                                        .attr("x2", function(d) {
+                                            return chart_scale(d.Augmented_APM + (2 * d.Augmented_APM_SE))
+                                        })
+                                        .attr("y1", 1 * (svg_chart_height / 4)) 
+                                        .attr("y2", 3 * (svg_chart_height / 4))
+                                        .attr("stroke", function(d) {
+                                            return color_scale(d.Augmented_APM);
+                                        })
 
-        var line_lower = chart_location
-                            .append("line")
-                            .attr("x1", function(d) {
-                                return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
-                            })
-                            .attr("x2", function(d) {
-                                return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
-                            })
-                            .attr("y1", 1 * (svg_chart_height / 4)) 
-                            .attr("y2", 3 * (svg_chart_height / 4))
-                            .attr("stroke", function(d) {
-                                return color_scale(d.Augmented_APM);
-                            })
+        var line_lower = chart_location.append("line")
+                                        .attr("x1", function(d) {
+                                            return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
+                                        })
+                                        .attr("x2", function(d) {
+                                            return chart_scale(d.Augmented_APM - (2 * d.Augmented_APM_SE))
+                                        })
+                                        .attr("y1", 1 * (svg_chart_height / 4)) 
+                                        .attr("y2", 3 * (svg_chart_height / 4))
+                                        .attr("stroke", function(d) {
+                                            return color_scale(d.Augmented_APM);
+                                        })
 
-        var circles = chart_location
-                        .append("circle")
-                        .attr("cx", function(d) {
-                            return chart_scale(d.Augmented_APM)
-                        })
-                        .attr("cy", svg_chart_height / 2)
-                        .attr("r", 5)
-                        .attr("fill", function(d) {
-                            return color_scale(d.Augmented_APM);
-                        })
-
+        var circles = chart_location.append("circle")
+                                    .attr("cx", function(d) {
+                                        return chart_scale(d.Augmented_APM)
+                                    })
+                                    .attr("cy", svg_chart_height / 2)
+                                    .attr("r", 5)
+                                    .attr("fill", function(d) {
+                                        return color_scale(d.Augmented_APM);
+                                    })
     });
 }
 
@@ -192,12 +174,11 @@ function sleep(seconds){
     while(new Date().getTime() < waitUntil) true;
 }
 
-// Update the underlying .csv file used in our table ---
+// Updates the table with the new .csv
 var update_csv = function() {
     league_val = document.getElementById("league").value;
     season_val = document.getElementById("season").value;
     filepath = "data/" + league_val + "_" + season_val + ".csv"
-    console.log("League: ", league_val, " Season: ", season_val, " Filepath: ", filepath);
 
     // Remove the previous table ---
     d3.selectAll("table").remove();
@@ -205,13 +186,11 @@ var update_csv = function() {
     create_rpm_table(filepath);
 }
 
-// Generate the default RPM 2017 EPL table ---
+// Initialize the table w/ the 2017 EPL results 
 create_rpm_table("data/epl_2017.csv?v=1.1")
 
-// Update the .csv table based on the selector values 
-d3
-.selectAll('.selector')
-.on('change', function() {
-    console.log("Changing the .csv!");
+// When selected changes, call the update .csv function 
+d3.selectAll('.selector')
+  .on('change', function() {
     update_csv();
-})
+  })
